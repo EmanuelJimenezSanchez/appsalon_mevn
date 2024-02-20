b0cbc2a62f65b9e9866694db53957fa48d9af34d
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2024-02-20 11:37:16 -0600

    Add new files and configurations

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose'
+import colors from 'colors'
 
 export const db = async () => {
   try {
-    const db = await mongoose.connect('mongodb+srv://root:HhXp9otsOIjDv0dY@cluster0.f02twmo.mongodb.net/?retryWrites=true&w=majority')
+    const db = await mongoose.connect(process.env.MONGO_URI)
 
-    console.log(`MongoDB connected: ${db.connection.host}`)
+    console.log(colors.cyan(`MongoDB connected: ${db.connection.host}`))
   } catch (error) {
     console.log('Error: ', error.message)
     process.exit(1)
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 // const express = require('express') // CommonJS
 import express from 'express' // ES6
+import dotenv from 'dotenv'
+import colors from 'colors'
 import { db } from './config/db.js'
 import servicesRoutes from './routes/servicesRoutes.js'
+
+// Variables de entorno
+dotenv.config()
 
 // Configurar la app
 const app = express()
@@ -17,5 +22,5 @@ const PORT = process.env.PORT || 4000
 
 //Arrancar la app
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log( colors.blue(`Server running on port ${PORT}`))
 })
