f09be7e932718c158c5b5f821fb0577353d273e2
Emanuel Jiménez <emanuel15300135@gmail.com>
2024-06-21 22:24:01 -0600

    Creación de ruta para actualizar servicios

--- backend/routes/servicesRoutes.js
+++ backend/routes/servicesRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { createService, getServices, getServiceById } from '../controllers/servicesCotroller.js'
+import { createService, getServices, getServiceById, updateService } from '../controllers/servicesCotroller.js'
 
 const router = express.Router()
 
 router.post('/', createService)
 router.get('/', getServices)
 router.get('/:id', getServiceById)
+router.put('/:id', updateService)
+
 
 export default router
