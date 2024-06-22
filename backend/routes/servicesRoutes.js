83a0918502dc48b8af9e385e8b549bbd7914b0a5
Emanuel Jiménez <emanuel15300135@gmail.com>
2024-06-21 21:59:14 -0600

    Ruta para obtener un servicio por id

--- backend/routes/servicesRoutes.js
+++ backend/routes/servicesRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createService, getServices } from '../controllers/servicesCotroller.js'
+import { createService, getServices, getServiceById } from '../controllers/servicesCotroller.js'
 
 const router = express.Router()
 
 router.post('/', createService)
 router.get('/', getServices)
+router.get('/:id', getServiceById)
 
 export default router
