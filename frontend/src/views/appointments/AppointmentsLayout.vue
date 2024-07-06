6f9c358830ed1c4defa8a2d4269292240ae2bbf2
Emanuel Jiménez <emanuel15300135@gmail.com>
2024-07-05 21:40:32 -0600

    Creación de rutas en el front para las citas y sus vistas

---
+++ frontend/src/views/appointments/AppointmentsLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterView } from 'vue-router';
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Appointment Layout</h1>
+  </div>
+
+  <main>
+    <RouterView />
+  </main>
+
+</template>
+
