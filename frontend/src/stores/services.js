5f00f940a3e7ebdeb9d3cf5e39593ec03bf036ba
Emanuel Jiménez <emanuel15300135@gmail.com>
2024-07-10 22:01:19 -0600

    Instalación de axios para consumo de la api y función all en store de services

--- frontend/src/stores/services.js
+++ frontend/src/stores/services.js
@@ -1,8 +1,17 @@
 import { ref, onMounted } from 'vue'
 import { defineStore } from 'pinia'
+import ServicesAPI from '../api/ServicesAPI'
 
 export const useServiceStore = defineStore('services', () => {
 
+  onMounted(async () => {
+    try {
+      const { data } = await ServicesAPI.all()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  })
 
   return {
 
--- frontend/src/views/appointments/ServicesView.vue
+++ frontend/src/views/appointments/ServicesView.vue
@@ -1,5 +1,6 @@
 <script setup>
-
+  import { useServiceStore } from '../../stores/services' 
+  const services = useServiceStore()
 </script>
 
 <template>
