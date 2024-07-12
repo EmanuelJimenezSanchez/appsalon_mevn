4abc9789d6d727f12e0a4cefbac5a7fbaa494287
Emanuel Jiménez <emanuel15300135@gmail.com>
2024-07-12 12:51:19 -0600

    Creación del componente ServiceItem

--- frontend/src/views/appointments/ServicesView.vue
+++ frontend/src/views/appointments/ServicesView.vue
@@ -1,11 +1,20 @@
 <script setup>
-  import { useServiceStore } from '../../stores/services' 
-  const services = useServiceStore()
+  import ServiceItem from '../../components/ServiceItem.vue'
+  import { useServiceStore } from '../../stores/services'
+  const store = useServiceStore()
 </script>
 
 <template>
-  <div>
-    <h1>Desde Servicios</h1>
+  <h2 class="text-4xl font-extrabold text-white mt-10">Servicios</h2>
+
+  <p class="text-white text-lg mt-5">A continuación elige al menos un servicio para tu cita</p>
+
+  <div class="grid grid-cols-2 gap-5 mt-5">
+    <ServiceItem
+      v-for="service in store.services"
+      :key="service._id"
+      :service="service"
+    />
   </div>
 </template>
 
